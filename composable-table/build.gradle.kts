1112cd1736f32b071ae6ef2fd884353dc5a0fd15
Sunny Chung <sunnychung@live.hk>
2024-02-11 11:32:47 +0800

    update composable-table to be multiplatform and update compose version to be 1.6.0-beat02

--- composable-table/build.gradle.kts
+++ composable-table/build.gradle.kts
@@ -1,7 +1,47 @@
+import org.jetbrains.kotlin.gradle.targets.js.dsl.ExperimentalWasmDsl
+
 plugins {
-    id("com.android.library")
-    kotlin("android")
+    kotlin("multiplatform")
+    id("org.jetbrains.compose")
     id("maven-publish")
+    id("com.android.library")
+//    kotlin("android")
+}
+
+kotlin {
+    jvm(name = "desktop") {
+        jvmToolchain(17)
+    }
+
+    @OptIn(ExperimentalWasmDsl::class)
+    wasmJs {
+
+    }
+
+    androidTarget {
+        compilations.all {
+            kotlinOptions {
+                jvmTarget = "1.8"
+            }
+        }
+    }
+
+    val iosTargets = listOf(
+        iosArm64(),
+        iosSimulatorArm64(),
+        iosX64(),
+    )
+
+    sourceSets {
+        val desktopMain by getting
+
+        commonMain.dependencies {
+            implementation(compose.runtime)
+            implementation(compose.foundation)
+            implementation(compose.material)
+            implementation(compose.ui)
+        }
+    }
 }
 
 android {
@@ -20,20 +60,17 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
     buildFeatures {
         compose = true
     }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.7"
-    }
+//    composeOptions {
+//        kotlinCompilerExtensionVersion = "1.4.7"
+//    }
 }
 
 dependencies {
-    implementation("androidx.compose.ui:ui:${libs.versions.jetpack.compose.get()}")
-    implementation("androidx.compose.foundation:foundation:${libs.versions.jetpack.compose.get()}")
+//    implementation("androidx.compose.ui:ui:${libs.versions.jetpack.compose.get()}")
+//    implementation("androidx.compose.foundation:foundation:${libs.versions.jetpack.compose.get()}")
 }
 
 publishing {
@@ -44,7 +81,7 @@ publishing {
             version = "1.1.0"
 
             afterEvaluate {
-                from(components["release"])
+//                from(components["release"])
             }
         }
     }
