893d27088658db25820e64c609d7430e061c2e12
Sunny Chung <sunnychung@live.hk>
2025-03-02 16:08:28 +0800

    update demo by adding clickables that modify the table data

--- demo/multiplatform-app/shared/src/commonMain/kotlin/com/sunnychung/lib/android/composabletable/demo/ux/AppView.kt
+++ demo/multiplatform-app/shared/src/commonMain/kotlin/com/sunnychung/lib/android/composabletable/demo/ux/AppView.kt
@@ -3,12 +3,18 @@ package com.sunnychung.lib.android.composabletable.demo.ux
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
 import androidx.compose.material.LocalTextStyle
 import androidx.compose.material.Surface
 import androidx.compose.material.Switch
@@ -17,6 +23,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateMapOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -81,7 +88,7 @@ fun AppContentView() {
     }
 
     @Composable
-    fun ContentCell(text: String, isChecked: Boolean, alignment: Alignment = Alignment.Center) {
+    fun ContentCell(text: String, isChecked: Boolean, alignment: Alignment = Alignment.Center, onClick: () -> Unit = {}) {
         Box(modifier = Modifier
             .background(
                 color = if (!isChecked) Color.White else Color.Yellow,
@@ -90,6 +97,7 @@ fun AppContentView() {
             .border(width = 1.dp, color = Color.Gray)
             .clickable {
                 // do something wonderful
+                onClick()
             }
         ) {
             Text(
@@ -103,6 +111,10 @@ fun AppContentView() {
 
     val checkedRows = remember {
         mutableStateMapOf<Int, Unit>()
+    }
+
+    val result: MutableList<TransitConnect> = remember {
+        mutableStateListOf(*RouteSearchHttpRepository().searchRoutes().toTypedArray())
     }
 
     @Composable
@@ -119,10 +131,6 @@ fun AppContentView() {
                 }
             )
         }
-    }
-
-    val result: List<TransitConnect> by remember {
-        mutableStateOf(RouteSearchHttpRepository().searchRoutes())
     }
 
     Column(horizontalAlignment = Alignment.CenterHorizontally) {
@@ -150,11 +158,12 @@ fun AppContentView() {
         )
         Table(
             modifier = Modifier
-                .fillMaxSize()
+                .fillMaxWidth()
+                .weight(1f)
                 .padding(10.dp),
             columnCount = headers.size,
             rowCount = result.size + 1,
-            stickyRowCount = 2,
+            stickyRowCount = 1,
             stickyColumnCount = 2,
             maxCellWidthDp = 320.dp
         ) { rowIndex, columnIndex ->
@@ -162,24 +171,50 @@ fun AppContentView() {
             if (rowIndex == 0) {
                 HeaderCell(header)
             } else {
-                val r = result[rowIndex - 1]
+                val row = rowIndex - 1
+                val r = result.elementAtOrNull(row) ?: return@Table
                 when (header) {
-                    "" -> ToggleCell(rowIndex - 1)
-                    "#" -> ContentCell(rowIndex.toString(), checkedRows.containsKey(rowIndex - 1))
-                    "Start Time" -> ContentCell(r.summary.startAt.formatHalfHoursMins(), checkedRows.containsKey(rowIndex - 1))
-                    "End Time" -> ContentCell(r.summary.endAt.formatHalfHoursMins(), checkedRows.containsKey(rowIndex - 1))
-                    "Duration" -> ContentCell(r.summary.duration().formatHoursMins(), checkedRows.containsKey(rowIndex - 1))
-                    "#Trips" -> ContentCell(r.summary.numOfTrips.toString(), checkedRows.containsKey(rowIndex - 1))
-                    "Total Fare" -> ContentCell(r.summary.totalFare.toString(), checkedRows.containsKey(rowIndex - 1))
-                    "Fare (JR)" -> ContentCell(r.summary.fares["JR"]?.toString() ?: "-", checkedRows.containsKey(rowIndex - 1))
-                    "Fare (Bus)" -> ContentCell(r.summary.fares["Bus"]?.toString() ?: "-", checkedRows.containsKey(rowIndex - 1))
-                    "Fare (Others)" -> ContentCell(r.summary.fares["Others"]?.toString() ?: "-", checkedRows.containsKey(rowIndex - 1))
-                    "Walking" -> ContentCell(SDuration(totalMs = toMillis(r.summary.walkingSeconds)).formatHoursMins(), checkedRows.containsKey(rowIndex - 1))
-                    "Waiting" -> ContentCell(SDuration(totalMs = toMillis(r.summary.waitingSeconds)).formatHoursMins(), checkedRows.containsKey(rowIndex - 1))
-                    "Stops" -> ContentCell(r.keyStopsFormatted(), checkedRows.containsKey(rowIndex - 1), Alignment.CenterStart)
+                    "" -> ToggleCell(row)
+                    "#" -> ContentCell(rowIndex.toString(), checkedRows.containsKey(row))
+                    "Start Time" -> ContentCell(r.summary.startAt.formatHalfHoursMins(), checkedRows.containsKey(row))
+                    "End Time" -> ContentCell(r.summary.endAt.formatHalfHoursMins(), checkedRows.containsKey(row))
+                    "Duration" -> ContentCell(r.summary.duration().formatHoursMins(), checkedRows.containsKey(row))
+                    "#Trips" -> ContentCell(r.summary.numOfTrips.toString(), checkedRows.containsKey(row))
+                    "Total Fare" -> ContentCell(r.summary.totalFare.toString(), checkedRows.containsKey(row))
+                    "Fare (JR)" -> ContentCell(r.summary.fares["JR"]?.toString() ?: "-", checkedRows.containsKey(row))
+                    "Fare (Bus)" -> ContentCell(r.summary.fares["Bus"]?.toString() ?: "-", checkedRows.containsKey(row))
+                    "Fare (Others)" -> ContentCell(r.summary.fares["Others"]?.toString() ?: "-", checkedRows.containsKey(row))
+                    "Walking" -> ContentCell(SDuration(totalMs = toMillis(r.summary.walkingSeconds)).formatHoursMins(), checkedRows.containsKey(row))
+                    "Waiting" -> ContentCell(SDuration(totalMs = toMillis(r.summary.waitingSeconds)).formatHoursMins(), checkedRows.containsKey(row))
+                    "Stops" -> ContentCell(r.keyStopsFormatted(), checkedRows.containsKey(row), Alignment.CenterStart) {
+                        // lengthen
+                        result[row].keyStops += result[row].keyStops
+                        if (!checkedRows.containsKey(row)) { // force the cell to be recomposed
+                            checkedRows[row] = Unit
+                        } else {
+                            checkedRows.remove(row)
+                        }
+                    }
                 }
             }
         }
+        Row(horizontalArrangement = Arrangement.spacedBy(10.dp)) {
+            Button(onClick = {
+                result.removeAt(result.lastIndex)
+            }) {
+                Text("Remove")
+            }
+
+            Button(onClick = {
+                if (result.isNotEmpty()) {
+                    result.add(result.last())
+                }
+            }) {
+                Text("Add")
+            }
+        }
+
+        Spacer(Modifier.height(10.dp))
     }
 }
 
